d019f7c0f11072bf7436142c1500daf79ae5f49e
Ryp <ryp.sqrt@gmail.com>
2014-05-22 16:50:57 +0200

    Update: Logger class added, issue raised on Cubieboard2 for gpio sysfs naming conventions

--- README.md
+++ README.md
@@ -1,3 +1,7 @@
 # OSAC
 
 OSAC stands for Open Source Access Control.
+
+# Known issues
+
+* Won't work with GCC 4.7 (no support for std::map::emplace for example)
---
+++ src/tools/log.hpp
@@ -0,0 +1,44 @@
+/**
+ * \file log.hpp
+ * \author Thibault Schueller <ryp.sqrt@gmail.com>
+ * \brief Log class declaration
+ */
+
+#ifndef LOG_HPP
+#define LOG_HPP
+
+#include <string>
+#include <iostream>
+#include <sstream>
+
+class LogLine
+{
+public:
+    LogLine(const char* file, const char* func, int line, std::ostream& out = std::cout)
+    : _out(out)
+    {
+        _stream << file << ':' << line << ": in function '" << func << "': ";
+    }
+
+    ~LogLine()
+    {
+        _stream << std::endl;
+        _out << _stream.rdbuf();
+        _out.flush();
+    }
+
+    template <class T>
+    LogLine& operator<<(const T& arg)
+    {
+        _stream << arg;
+        return (*this);
+    }
+
+private:
+    std::stringstream   _stream;
+    std::ostream&       _out;
+};
+
+#define LOG() LogLine(__FILE__, __FUNCTION__, __LINE__)
+
+#endif // LOG_HPP
