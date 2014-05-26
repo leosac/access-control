54437a8bc809dc1fdf9faea1cca45e2f9c3eb8bd
Ryp <ryp.sqrt@gmail.com>
2014-05-26 11:30:02 +0200

    Update: auth csn handling
    Former-commit-id: abcb2da0f093845636b7fc99a5d5d80f3510ee24
    Former-commit-id: 1dfa0f0c162546024dbe27d99979f6710a4633d7

--- src/modules/rpleth/auth/authenticator.cpp
+++ src/modules/rpleth/auth/authenticator.cpp
@@ -6,20 +6,74 @@
 
 #include "authenticator.hpp"
 
+#include <sstream>
+#include <iomanip>
+
 void Authenticator::serialize(boost::property_tree::ptree& node)
 {
     boost::property_tree::ptree& child = node.add("auth", std::string());
 
-    child.put("user", "test"); // TODO
+    child.put("user", "test"); // FIXME
+    for (const auto& csn : _csnList)
+    {
+        boost::property_tree::ptree ptre;
+
+        ptre.put("", serializeCard(csn));
+        child.add_child("card", ptre);
+    }
 }
 
 void Authenticator::deserialize(boost::property_tree::ptree& node)
 {
-    for (auto& v : node)
+    for (const auto& v : node)
     {
-        if (v.first == "auth")
-        {
-            // TODO
-        }
+        if (v.first == "card")
+            deserializeCard(v.second.data());
     }
+//     CSN test;
+//     test.push_back(Byte());
+//     test.push_back(Byte());
+//     test.push_back(Byte());
+//     test.push_back(Byte());
+//     authorizeCard(test);
+}
+
+bool Authenticator::hasAccess(const CSN& csn)
+{
+    if (std::find(_csnList.begin(), _csnList.end(), csn) != _csnList.end())
+        return (true);
+    else
+        return (false);
+}
+
+void Authenticator::authorizeCard(const Authenticator::CSN& csn)
+{
+    _csnList.push_back(csn);
+}
+
+std::string Authenticator::serializeCard(const Authenticator::CSN& csn)
+{
+    std::ostringstream  oss;
+
+    for (std::size_t i = 0; i < csn.size(); ++i)
+    {
+        if (i > 0)
+            oss << ':';
+        oss << std::setw(2) << std::setfill('0') << std::hex << static_cast<unsigned>(csn[i]);
+    }
+    return (oss.str());
+}
+
+void Authenticator::deserializeCard(const std::string& card)
+{
+    std::string         csnString(card);
+    std::istringstream  iss;
+    CSN                 csn;
+    unsigned            byte;
+
+    std::replace(csnString.begin(), csnString.end(), ':', ' ');
+    iss.str(csnString);
+    while (iss >> std::hex >> byte)
+        csn.push_back(byte);
+    authorizeCard(csn);
 }
