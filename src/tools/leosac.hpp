fbb8e6ff64fffed958408d07ea694d673c4c4a56
Ryp <ryp.sqrt@gmail.com>
2014-06-27 17:13:43 +0200

    Update: Leosac.cpp refactor, improved platform support, new cubieboard hardware config.
    Former-commit-id: 5834746521c12124da6f41b5610814aebaf7706c
    Former-commit-id: 7070ba4616a4025887b705535643c5ccd73ab929

--- src/tools/leosac.hpp
+++ src/tools/leosac.hpp
@@ -10,33 +10,44 @@
 #include <string>
 
 #ifndef LEOSAC_VERSION_MAJOR
-#define LEOSAC_VERSION_MAJOR 0
+#   define LEOSAC_VERSION_MAJOR 0
 #endif
 
 #ifndef LEOSAC_VERSION_MINOR
-#define LEOSAC_VERSION_MINOR 0
+#   define LEOSAC_VERSION_MINOR 0
 #endif
 
 #ifndef LEOSAC_VERSION_PATCH
-#define LEOSAC_VERSION_PATCH 0
+    #define LEOSAC_VERSION_PATCH 0
 #endif
 
-class LEOSAC
-{
-    LEOSAC() = delete;
+#if defined(LEOSAC_PLATFORM_RASPI)
+#   define LEOSAC_PLATFORM (LeosacPlatform::RaspberryPiRev2)
+#elif defined(LEOSAC_PLATFORM_CUBIEBOARD)
+#   define LEOSAC_PLATFORM (LeosacPlatform::CubieBoard)
+#else
+#   define LEOSAC_PLATFORM (LeosacPlatform::Unknown)
+#endif
 
-public:
+namespace Leosac
+{
     static const int Major = LEOSAC_VERSION_MAJOR;
     static const int Minor = LEOSAC_VERSION_MINOR;
     static const int Patch = LEOSAC_VERSION_PATCH;
 
-public:
+    enum class LeosacPlatform {
+        Unknown = 0,
+        RaspberryPiRev2,
+        CubieBoard
+    };
+    static const LeosacPlatform Platform = LEOSAC_PLATFORM;
+
     /**
      * return LEOSAC version string formatted this way: MAJOR.MINOR.PATCH
      * @return version string
      * @see versionCompare()
      */
-    static std::string& getVersionString();
+    std::string&    getVersionString();
 
     /**
      * compare version string against current LEOSAC version
@@ -44,7 +55,12 @@ public:
      * @return 0 on match, -1 if v older than LEOSAC, 1 if v is newer than LEOSAC
      * @see getVersionString()
      */
-    static int          versionCompare(const std::string& v);
+    int             versionCompare(const std::string& v);
 };
+
+#undef LEOSAC_VERSION_MAJOR
+#undef LEOSAC_VERSION_MINOR
+#undef LEOSAC_VERSION_PATCH
+#undef LEOSAC_PLATFORM
 
 #endif // LEOSAC_HPP
