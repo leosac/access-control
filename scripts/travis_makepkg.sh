bca8aad7eaa3b03ca40ee3f139d6b1eb96beddf3
Ryp <ryp.sqrt@gmail.com>
2014-08-26 16:34:25 +0200

    updated travis script
    Former-commit-id: 25bfc65b55cdd59ed74854d8e9e3e475ce9288a7
    Former-commit-id: 933673fb0466db6ba64766b1c682e8def5b0bbe5

--- scripts/travis_makepkg.sh
+++ scripts/travis_makepkg.sh
@@ -1,12 +1,14 @@
 #!/bin/bash
 
 set -e
+set -x
 
 test ! -d debian
 mkdir debian
 
 dch --create -v "${TRAVIS_TAG}-1" --package leosac "Travis auto-generated package (commit ${TRAVIS_COMMIT})" # Initial changelog
 dch -r "Unforeseen Consequences" # Mark as release
+cat debian/changelog
 echo "9" > debian/compat
 cp pkg/deb/control      debian
 cp pkg/deb/rules        debian
