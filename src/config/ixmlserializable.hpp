c03824d37bbcf469d6dcac1a0bdf5f817675b9c1
xaqq <kapp.arno@gmail.com>
2014-09-22 16:38:37 +0200

    add some comments

--- src/config/ixmlserializable.hpp
+++ src/config/ixmlserializable.hpp
@@ -11,11 +11,29 @@
 
 using boost::property_tree::ptree;
 
+/**
+* Interface that defines a Serializable object.
+*
+* Objects implementing this interface must be able to:
+*      1. Serialize themselves into a property tree.
+*      2. Unserialize and update their state / config from a given property tree.
+*
+* This is useful for loading object using configuration file, and to save an object's configuration
+* into a property tree.
+*/
 class IXmlSerializable
 {
 public:
     virtual ~IXmlSerializable() {}
+
+    /**
+    * Fill the property tree with relevant attributes.
+    */
     virtual void    serialize(ptree& node) = 0;
+
+    /**
+    * The implementation shall update the object's status with relevant data from the property tree.
+    */
     virtual void    deserialize(const ptree& node) = 0;
 };
 
