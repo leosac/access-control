79e1b3b6aaa81e253405d4ff07749a82b46be3cf
xaqq <kapp.arno@gmail.com>
2014-09-29 10:43:22 +0200

    Prepare for support of PifaceDigital device
    Former-commit-id: 6ae413a955451ed18c4686e054de88a910ad013b
    Former-commit-id: 4e774c4f461dffbb9112fe6f3331d2c4ed3aadd0

---
+++ src/hardware/device/gpio/pfdigital.hpp
@@ -0,0 +1,11 @@
+/**
+* Provide access to GPIO managed by the PiFace Digital card.
+*
+* This could inherit from IGPIOProvide, but we'll that out for now, at least.
+*/
+class PFDigital
+{
+public:
+    void poll();
+
+};
