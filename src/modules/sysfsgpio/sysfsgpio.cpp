9ff3bfa5b5f4c576007222df0915265c36343160
Kapp Arnaud <kapp.arno@gmail.com>
2014-10-05 01:44:10 +0200

    untested support of write to sysfsgpio

--- src/modules/sysfsgpio/sysfsgpio.cpp
+++ src/modules/sysfsgpio/sysfsgpio.cpp
@@ -1,6 +1,9 @@
 #include <memory>
 #include <zmqpp/message.hpp>
 #include <boost/property_tree/ptree.hpp>
+#include <tools/log.hpp>
+#include <zmqpp/context.hpp>
+#include <fcntl.h>
 #include "sysfsgpio.hpp"
 #include "zmqpp/actor.hpp"
 
@@ -37,6 +40,12 @@ config_(config),
 is_running_(true),
 ctx_(ctx)
     {
+    process_config(config);
+
+    for (auto &gpio : gpios_)
+        {
+        reactor_.add(gpio.sock_, std::bind(&SysFsGpioPin::handle_message, &gpio));
+        }
     reactor_.add(pipe_, std::bind(&SysFsGpioModule::handle_pipe, this));
     }
 
@@ -55,4 +64,92 @@ void SysFsGpioModule::handle_pipe()
     pipe_.receive(s, true);
     if (s == zmqpp::signal::stop)
         is_running_ = false;
+    }
+
+void SysFsGpioModule::process_config(const boost::property_tree::ptree &cfg)
+    {
+    boost::property_tree::ptree module_config = cfg.get_child("module_config");
+
+    for (auto &node : module_config.get_child("gpios"))
+        {
+        boost::property_tree::ptree gpio_cfg = node.second;
+
+        std::string gpio_name = gpio_cfg.get_child("name").data();
+        int gpio_no = std::stoi(gpio_cfg.get_child("no").data());
+        std::string gpio_direction = gpio_cfg.get_child("direction").data();
+
+        LOG() << "Creating GPIO " << gpio_name << ", with no " << gpio_no << ". direction = " << gpio_direction;
+
+        export_gpio(gpio_no);
+        SysFsGpioPin pin(ctx_, gpio_name, gpio_no);
+
+        pin.set_direction(gpio_direction);
+        gpios_.push_back(std::move(pin));
+        }
+    }
+
+SysFsGpioPin::SysFsGpioPin(zmqpp::context &ctx, const std::string &name, int gpio_no) :
+sock_(ctx, zmqpp::socket_type::rep)
+    {
+    sock_.bind("inproc://" + name);
+    std::string full_path = "/sys/class/gpio" + std::to_string(gpio_no);
+    file_fd_ = open(full_path.c_str(), O_RDONLY | O_NONBLOCK);
+    assert(file_fd_ != -1);
+    }
+
+SysFsGpioPin::~SysFsGpioPin()
+    {
+    assert(::close(file_fd_));
+    }
+
+void SysFsGpioModule::export_gpio(int gpio_no)
+    {
+    UnixFs::writeSysFsValue("/sys/class/gpio/export", gpio_no);
+    }
+
+SysFsGpioPin::SysFsGpioPin(SysFsGpioPin &&o) :
+sock_(std::move(o.sock_))
+    {
+    this->file_fd_ = o.file_fd_;
+    }
+
+void SysFsGpioPin::set_direction(const std::string &direction)
+    {
+    UnixFs::writeSysFsValue("/sys/class/gpio" + std::to_string(gpio_no_) + "/direction", direction);
+    }
+
+void SysFsGpioPin::handle_message()
+    {
+    zmqpp::message_t msg;
+    std::string frame1;
+    sock_.receive(msg);
+
+    msg >> frame1;
+    bool ok = false;
+    if (frame1 == "ON")
+        ok = turn_on();
+    else if (frame1 == "OFF")
+        ok = turn_off();
+    else if (frame1 == "TOGGLE")
+        ok = toggle();
+    sock_.send(ok ? "OK" : "KO");
+    }
+
+bool SysFsGpioPin::turn_on()
+    {
+    UnixFs::writeSysFsValue("/sys/class/gpio" + std::to_string(gpio_no_) + "/value", 1);
+    return true;
+    }
+
+bool SysFsGpioPin::turn_off()
+    {
+    UnixFs::writeSysFsValue("/sys/class/gpio" + std::to_string(gpio_no_) + "/value", 0);
+    return true;
+    }
+
+bool SysFsGpioPin::toggle()
+    {
+    int v = UnixFs::readSysFsValue<int>("/sys/class/gpio" + std::to_string(gpio_no_) + "/value");
+    UnixFs::writeSysFsValue("/sys/class/gpio" + std::to_string(gpio_no_) + "/value", v == 1 ? 0 : 1);
+    return true;
     }
--- src/modules/sysfsgpio/sysfsgpio.hpp
+++ src/modules/sysfsgpio/sysfsgpio.hpp
@@ -6,6 +6,59 @@
 #include <zmqpp/reactor.hpp>
 
 /**
+* This is a implementation class. It's not exposed to the user and is for this
+* module internal code only.
+*
+* It abstract a GPIO pin.
+*/
+struct SysFsGpioPin
+    {
+    SysFsGpioPin(zmqpp::context &ctx, const std::string &name, int gpio_no);
+    ~SysFsGpioPin();
+
+    SysFsGpioPin(const SysFsGpioPin &) = delete;
+    SysFsGpioPin &operator=(const SysFsGpioPin &) = delete;
+
+    SysFsGpioPin(SysFsGpioPin &&o);
+
+
+    void set_direction(const std::string &direction);
+
+    /**
+    * Write to sysfs to turn the gpio on.
+    */
+    bool turn_on();
+
+    /**
+    * Write to sysfs to turn the gpio on.
+    */
+    bool turn_off();
+
+    /**
+    * Read to sysfs and then write the opposite value
+    */
+    bool toggle();
+
+    /**
+    * The SysFsGpioModule will register this method so its called when a message
+    * is ready on the pin socket.
+    */
+    void handle_message();
+
+    /**
+    * File descriptor of the GPIO in sysfs.
+    */
+    int file_fd_;
+
+    int gpio_no_;
+
+    /**
+    * listen to command from other component.
+    */
+    zmqpp::socket sock_;
+    };
+
+/**
 * Handle GPIO management over sysfs.
 */
 class SysFsGpioModule
@@ -37,5 +90,18 @@ private:
 
     //void handle_
 
+    /**
+    * Process the configuration, preparing configured GPIO pin.
+    */
+    void process_config(const boost::property_tree::ptree &cfg);
+
+
+    /**
+    * Write to "gpio_export_path" so the kernel export the socket to sysfs.
+    */
+    void export_gpio(int gpio_no);
+
     zmqpp::context &ctx_;
+
+    std::vector<SysFsGpioPin> gpios_;
     };
