7d6a5902c05989f3928418983c19a8e3eece12c0
xaqq <kapp.arno@gmail.com>
2014-10-07 14:16:16 +0200

    formatting and module shutdown order
    Former-commit-id: 8ead6fb112b9452bfc27c055e73e4cf100e4ea56
    Former-commit-id: a74112479b45d18be5050c9d227a3f16fbb71974

--- src/core/zmodule_manager.cpp
+++ src/core/zmodule_manager.cpp
@@ -9,32 +9,34 @@
 
 void zModuleManager::unloadLibraries()
 {
-    for (auto& module_info : BLABLA_)
+    for (auto &module_info : BLABLA_)
     {
-        try {
+        try
+        {
             module_info.lib_->close();
         }
-        catch (const DynLibException& e) {
+        catch (const DynLibException &e)
+        {
             throw (ModuleException(e.what()));
         }
         //delete module_info.lib_;
-       // module_info.lib_ = nullptr;
+        // module_info.lib_ = nullptr;
     }
     BLABLA_.clear();
 }
 
 
 bool zModuleManager::initModules()
+{
+    for (auto &module_info : BLABLA_)
     {
-    for (auto & module_info : BLABLA_)
-        {
         try
-            {
+        {
             void *symptr = module_info.lib_->getSymbol("start_module");
             assert(symptr);
-            std::function< bool (zmqpp::socket *) > actor_fun = std::bind(
-                    ((bool (*)(zmqpp::socket *, boost::property_tree::ptree, zmqpp::context & )) symptr),
-            std::placeholders::_1, // placeholder for pipe
+            std::function<bool(zmqpp::socket * )> actor_fun = std::bind(
+                    ((bool (*)(zmqpp::socket *, boost::property_tree::ptree, zmqpp::context &)) symptr),
+                    std::placeholders::_1, // placeholder for pipe
                     module_info.config_,
                     std::ref(ctx_));
             zmqpp::actor new_module(actor_fun);
@@ -42,81 +44,84 @@ bool zModuleManager::initModules()
 
             LOG() << "Module {" << module_info.name_ << "} initialized. (level = " <<
                     module_info.config_.get<int>("level", 100);
-            }
+        }
         catch (std::exception &e)
-            {
+        {
             LOG() << "Unable to init module {" << module_info.name_ << "}: " << e.what();
             return false;
-            }
         }
-    return true;
     }
+    return true;
+}
 
 void zModuleManager::addToPath(const std::string &dir)
-    {
+{
     if (std::find(path_.begin(), path_.end(), dir) == path_.end())
         path_.push_back(dir);
-    }
+}
 
 bool zModuleManager::loadModule(const boost::property_tree::ptree &cfg)
-    {
+{
     std::string filename = cfg.get_child("file").data();
     std::string module_name = cfg.get_child("name").data();
 
     LOG() << "Attempting to load module nammed " << module_name << " (shared lib file = " << filename << ")";
     for (const std::string &path_entry : path_)
-        {
+    {
         // fixme not clean enough.
         if (UnixFs::fileExists(path_entry + "/" + filename))
-            {
-                ModuleInfo module_info;
+        {
+            ModuleInfo module_info;
 
-                module_info.name_ = module_name;
-                module_info.config_ = cfg;
-                if (!(module_info.lib_ = load_library_file(path_entry + "/" + filename)))
-                    return false;
-                BLABLA_.insert(module_info);
+            module_info.name_ = module_name;
+            module_info.config_ = cfg;
+            if (!(module_info.lib_ = load_library_file(path_entry + "/" + filename)))
+                return false;
+            BLABLA_.insert(module_info);
             LOG() << "library file loaded (not init yet)";
             return true;
-            }
-
         }
+
+    }
     LOG() << "Could'nt load this module";
     return false;
-    }
+}
 
 DynamicLibrary *zModuleManager::load_library_file(const std::string &full_path)
-    {
-    DynamicLibrary* lib = nullptr;
+{
+    DynamicLibrary *lib = nullptr;
 
     LOG() << "Loading library at: " << full_path;
     lib = new DynamicLibrary(full_path);
-    try {
+    try
+    {
         lib->open(DynamicLibrary::RelocationMode::Now);
     }
-    catch (const DynLibException& e) {
+    catch (const DynLibException &e)
+    {
         delete lib;
         return nullptr;
     }
-        return lib;
-    }
+    return lib;
+}
 
 void zModuleManager::stopModules()
-    {
+{
+    std::reverse(modules_.begin(), modules_.end());
     for (zmqpp::actor &a : modules_)
-        {
+    {
         a.stop(true);
-        }
     }
+}
 
 zModuleManager::~zModuleManager()
-    {
+{
     stopModules();
     unloadLibraries();
-    }
+}
 
 zModuleManager::zModuleManager(zmqpp::context &ctx) :
-ctx_(ctx)
-    {
+        ctx_(ctx)
+{
 
-    }
+}
