61d71f9b9a6a75440eeef7d7abb538aba820ff5b
xaqq <kapp.arno@gmail.com>
2014-10-13 14:38:36 +0200

    Introducing a base module class. this should help with #34
    Former-commit-id: 5ef91846c9024b7a7a48ae67bb088dc2a937a098
    Former-commit-id: d2bc177d0a024414edafd0ede72ec691e43d3262

---
+++ src/modules/BaseModule.cpp
@@ -0,0 +1,32 @@
+#include "BaseModule.hpp"
+
+BaseModule::BaseModule(zmqpp::context &ctx,
+        zmqpp::socket *pipe,
+        boost::property_tree::ptree const &cfg) :
+ctx_(ctx),
+pipe_(*pipe),
+config_(cfg)
+{
+    reactor_.add(pipe_, std::bind(&BaseModule::handle_pipe, this));
+}
+
+void BaseModule::run()
+{
+    while (is_running_)
+    {
+        reactor_.poll();
+    }
+}
+
+void BaseModule::handle_pipe()
+{
+    zmqpp::signal sig;
+    pipe_.receive(sig);
+
+    if (sig == zmqpp::signal::stop)
+    {
+        is_running_ = false;
+    }
+    else
+        assert(0);
+}
