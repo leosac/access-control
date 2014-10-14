a569f9b8e66655daf4c49170971dfec3851b0ab2
xaqq <kapp.arno@gmail.com>
2014-10-14 16:26:52 +0200

    prepare wiegand reader to support a led and buzzer device
    Former-commit-id: 0f444e7ca1a9bb20d53bf994c05cda6da01c3c41
    Former-commit-id: 323755fd9786c0338eff82c437752f13e321fedf

--- src/modules/wiegand/wiegand.hpp
+++ src/modules/wiegand/wiegand.hpp
@@ -12,6 +12,21 @@
 *     1. Upon reception of what looks like a full wiegand frame, a message MUST be published to the application bus.
 *     2. The message must be published on the "S_{READER_NAME}" topic.
 *     3. This message SHALL contain the card-id that was read, in hexadecimal format, for example: "40:a0:83:80"
+*
+* ### Configuration Options:
+*
+*  Module configuration shall be under the `module_config` tag.
+*
+* Options    | Options  | Options    | Description                                            | Mandatory
+* -----------|----------|------------|--------------------------------------------------------|-----------
+* readers    |          |            | Lists of all configured readers                        | YES
+* ---->      | reader   |            | Configuration of 1 wiegand reader                      | YES
+* ---->      | ---->    | name       | device name                                            | YES
+* ---->      | ---->    | high       | name of the input GPIO that sends "high" data          | YES
+* ---->      | ---->    | low        | name of the input GPIO that sends "low" data           | YES
+* ---->      | ---->    | green_led  | name of the green led device attached to the reader    | NO
+* ---->      | ---->    | buzzer     | name of the buzzer device attached to the reader       | NO
+*
 */
 class WiegandReaderModule
 {
