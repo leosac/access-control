0531477b99826bbb24895f5d18cc665d74459ea4
xaqq <kapp.arno@gmail.com>
2014-10-14 16:59:33 +0200

    refactor testing code by adding a base TestHelper class

--- test/Led.cpp
+++ test/Led.cpp
@@ -5,124 +5,111 @@
 #include "hardware/device/FLED.hpp"
 
 
-class LedTest : public ::testing::Test
+class LedTest : public TestHelper
 {
 private:
-    // starts the led module
-    bool run_module(zmqpp::socket *pipe)
-    {
-        boost::property_tree::ptree cfg, module_cfg, leds_cfg, led1_cfg;
+  // starts the led module
+  virtual bool run_module(zmqpp::socket *pipe) override
+  {
+    boost::property_tree::ptree cfg, module_cfg, leds_cfg, led1_cfg;
 
-        led1_cfg.add("name", "my_led");
-        led1_cfg.add("gpio", "my_gpio");
-        led1_cfg.add("default_blink_duration", "1000");
-        led1_cfg.add("default_blink_speed", "100");
+    led1_cfg.add("name", "my_led");
+    led1_cfg.add("gpio", "my_gpio");
+    led1_cfg.add("default_blink_duration", "1000");
+    led1_cfg.add("default_blink_speed", "100");
 
-        leds_cfg.add_child("led", led1_cfg);
-        module_cfg.add_child("leds", leds_cfg);
-        cfg.add_child("module_config", module_cfg);
+    leds_cfg.add_child("led", led1_cfg);
+    module_cfg.add_child("leds", leds_cfg);
+    cfg.add_child("module_config", module_cfg);
 
-        return test_run_module<LEDModule>(&ctx_, pipe, cfg);
-    }
+    return test_run_module<LEDModule>(&ctx_, pipe, cfg);
+  }
 
 public:
 
-    LedTest() :
-    ctx_
-            (),
-    bus_(ctx_),
-    bus_sub_(ctx_, zmqpp::socket_type::sub),
+  LedTest() :
+    TestHelper(),
     gpio_(ctx_, "my_gpio"),
-    gpio_actor_(std::bind(&FakeGPIO::run, &gpio_, std::placeholders::_1)),
-    module_actor_(std::bind(&LedTest::run_module, this, std::placeholders::_1))
-{
-    bus_sub_.connect("inproc://zmq-bus-pub");
+    gpio_actor_(std::bind(&FakeGPIO::run, &gpio_, std::placeholders::_1))
+  {
     bus_sub_.subscribe("");
-}
+  }
 
-    ~LedTest()
-    {
-    }
+  ~LedTest()
+  {
+  }
+  FakeGPIO gpio_;
 
-    zmqpp::context ctx_;
-    MessageBus bus_;
-    // subscribe socket to the bus
-    zmqpp::socket bus_sub_;
-
-    FakeGPIO gpio_;
-    // to allow the fake gpio to react to command.
-    zmqpp::actor gpio_actor_;
-
-    // run the module code (like it was loaded by core)
-    zmqpp::actor module_actor_;
+  // to allow the fake gpio to react to command.
+  zmqpp::actor gpio_actor_;
 };
 
 TEST_F(LedTest, turnOn)
 {
-// create a facade
-FLED my_led(ctx_, "my_led");
-ASSERT_TRUE(my_led.isOff());
+  // create a facade
+  FLED my_led(ctx_, "my_led");
+  ASSERT_TRUE(my_led.isOff());
 
-ASSERT_TRUE(my_led.turnOn());
-ASSERT_TRUE(my_led.isOn());
-ASSERT_TRUE(bus_read(bus_sub_, "S_my_gpio", "ON"));
+  ASSERT_TRUE(my_led.turnOn());
+  ASSERT_TRUE(my_led.isOn());
+  ASSERT_TRUE(bus_read(bus_sub_, "S_my_gpio", "ON"));
 }
 
 TEST_F(LedTest, turnOff)
 {
-FLED my_led(ctx_, "my_led");
-ASSERT_TRUE(my_led.isOff());
+  FLED my_led(ctx_, "my_led");
+  ASSERT_TRUE(my_led.isOff());
 
-ASSERT_TRUE(my_led.turnOn());
-ASSERT_TRUE(my_led.isOn());
-ASSERT_TRUE(bus_read(bus_sub_, "S_my_gpio", "ON"));
+  ASSERT_TRUE(my_led.turnOn());
+  ASSERT_TRUE(my_led.isOn());
+  ASSERT_TRUE(bus_read(bus_sub_, "S_my_gpio", "ON"));
 
-ASSERT_TRUE(my_led.turnOff());
-ASSERT_TRUE(bus_read(bus_sub_, "S_my_gpio", "OFF"));
-ASSERT_TRUE(my_led.isOff());
+  ASSERT_TRUE(my_led.turnOff());
+  ASSERT_TRUE(bus_read(bus_sub_, "S_my_gpio", "OFF"));
+  ASSERT_TRUE(my_led.isOff());
 }
 
 TEST_F(LedTest, toggle)
 {
-FLED my_led(ctx_, "my_led");
-ASSERT_TRUE(my_led.isOff());
+  FLED my_led(ctx_, "my_led");
+  ASSERT_TRUE(my_led.isOff());
 
-// fake gpio start off
-ASSERT_TRUE(my_led.toggle());
-ASSERT_TRUE(bus_read(bus_sub_, "S_my_gpio", "ON"));
-ASSERT_TRUE(my_led.isOn());
+  // fake gpio start off
+  ASSERT_TRUE(my_led.toggle());
+  ASSERT_TRUE(bus_read(bus_sub_, "S_my_gpio", "ON"));
+  ASSERT_TRUE(my_led.isOn());
 
-ASSERT_TRUE(my_led.toggle());
-ASSERT_TRUE(bus_read(bus_sub_, "S_my_gpio", "OFF"));
-ASSERT_TRUE(my_led.isOff());
+  ASSERT_TRUE(my_led.toggle());
+  ASSERT_TRUE(bus_read(bus_sub_, "S_my_gpio", "OFF"));
+  ASSERT_TRUE(my_led.isOff());
 }
 
 TEST_F(LedTest, blink)
 {
-FLED my_led(ctx_, "my_led");
-ASSERT_TRUE(my_led.isOff());
-ASSERT_TRUE(my_led.state().st == FLED::State::OFF);
-ASSERT_FALSE(my_led.isBlinking());
+  FLED my_led(ctx_, "my_led");
+  ASSERT_TRUE(my_led.isOff());
+  ASSERT_TRUE(my_led.state().st == FLED::State::OFF);
+  ASSERT_FALSE(my_led.isBlinking());
 
-my_led.blink(10, 1);
-ASSERT_TRUE(my_led.isBlinking());
-ASSERT_TRUE(my_led.state().st == FLED::State::BLINKING);
+  my_led.blink(10, 1);
+  ASSERT_TRUE(my_led.isBlinking());
+  ASSERT_TRUE(my_led.state().st == FLED::State::BLINKING);
 
-// we should see 10 changes
-for (int i = 0; i < 5; ++i)
-{
-ASSERT_TRUE(bus_read(bus_sub_, "S_my_gpio", "ON"));
-ASSERT_TRUE(my_led.isBlinking());
-ASSERT_TRUE(bus_read(bus_sub_, "S_my_gpio", "OFF"));
-}
+  // we should see 10 changes
+  for (int i = 0; i < 5; ++i)
+    {
+      ASSERT_TRUE(bus_read(bus_sub_, "S_my_gpio", "ON"));
+      ASSERT_TRUE(my_led.isBlinking());
+      ASSERT_TRUE(bus_read(bus_sub_, "S_my_gpio", "OFF"));
+    }
 
-// the "final" off: sent when blinking is over (even if gpio is off already)
-ASSERT_TRUE(bus_read(bus_sub_, "S_my_gpio", "OFF"));
-ASSERT_TRUE(my_led.isOff());
+  // the "final" off: sent when blinking is over (even if gpio is off already)
+  ASSERT_TRUE(bus_read(bus_sub_, "S_my_gpio", "OFF"));
+  ASSERT_TRUE(my_led.isOff());
 
-//std::this_thread::sleep_for(std::chrono::milliseconds(100));
-ASSERT_FALSE(my_led.isBlinking());
-ASSERT_TRUE(my_led.isOff());
-ASSERT_TRUE(my_led.state().st == FLED::State::OFF);
+  //std::this_thread::sleep_for(std::chrono::milliseconds(100));
+  ASSERT_FALSE(my_led.isBlinking());
+  ASSERT_TRUE(my_led.isOff());
+  ASSERT_TRUE(my_led.state().st == FLED::State::OFF);
 
 }
--- test/helper/TestHelper.hpp
+++ test/helper/TestHelper.hpp
@@ -22,7 +22,7 @@ bool test_run_module(zmqpp::context *ctx, zmqpp::socket *pipe, const boost::prop
 /**
 * Part of the `bus_read()` stuff.
 */
-bool bus_read_extract(zmqpp::message *m)
+bool bus_read_extract(zmqpp::message * m)
 {
     return true;
 }
@@ -45,7 +45,7 @@ bool bus_read_extract(zmqpp::message *m, T first_arg, Content... content)
 * Frame extraction method specialized (thanks to overloading) for `const char *`
 */
 template<typename ...Content>
-bool bus_read_extract(zmqpp::message *m, const char * first_arg, Content... content)
+bool bus_read_extract(zmqpp::message *m, const char *first_arg, Content... content)
 {
     std::string value;
     *m >> value;
@@ -68,3 +68,60 @@ bool bus_read(zmqpp::socket &sub, Content... content)
         return false;
     return bus_read_extract(&msg, content...);
 }
+
+/**
+* Base class for test fixtures, it defines a ZMQ context, a BUS and a sub socket connect to the bus.
+*/
+class TestHelper : public ::testing::Test
+{
+private:
+    /**
+    * Called in the module's actor's thread.
+    * Override this: Construct configuration for the module then `run_test_module()` with appropriate parameters.
+    */
+    virtual bool run_module(zmqpp::socket *pipe) = 0;
+
+public:
+
+    TestHelper() :
+            ctx_(),
+            bus_(ctx_),
+            bus_sub_(ctx_, zmqpp::socket_type::sub)
+    {
+        bus_sub_.connect("inproc://zmq-bus-pub");
+    }
+
+    virtual ~TestHelper()
+    {
+        delete module_actor_;
+    }
+
+    /**
+    * We need this 2-step initialization to prevent calling virtual method (run_module) in constructor.
+    */
+    virtual void SetUp() override final
+    {
+        module_actor_ = new zmqpp::actor(std::bind(&TestHelper::run_module, this, std::placeholders::_1));
+    }
+
+    /**
+    * A context for the test case
+    */
+    zmqpp::context ctx_;
+
+    /**
+    * A BUS identical to the one spawned by core.
+    */
+    MessageBus bus_;
+
+    /**
+    * A SUB socket connected to the previous bus.
+    */
+    zmqpp::socket bus_sub_;
+
+    /**
+    * An actor, to run the module code the same way it would be run by the core.
+    * Does NOT subscribe to anything.
+    */
+    zmqpp::actor *module_actor_;
+};
