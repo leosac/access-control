d7e18d6100ec9687eabe3b5bd2910941864ba22f
xaqq <kapp.arno@gmail.com>
2014-10-22 19:27:20 +0200

    Code for Logging.
    Copy-pasted some logging macros that needs some fix.
    This commit introduce ugly code.
    
    
    Former-commit-id: d293ec9dbec48e631fedb167b0cc3444a1e73d62
    Former-commit-id: ba96e6a7e5d6ef51d87fe783a0224e2255c26f65

--- src/modules/sysfsgpio/init.cpp
+++ src/modules/sysfsgpio/init.cpp
@@ -15,19 +15,31 @@ extern "C" __attribute__((visibility("default"))) bool start_module(zmqpp::socke
         boost::property_tree::ptree cfg,
         zmqpp::context &zmq_ctx)
 {
-    SysFsGpioModule module(cfg, pipe, zmq_ctx);
+    try
+    {
+        INFO("Hello World");
+        SysFsGpioModule module(cfg, pipe, zmq_ctx);
 
-    std::cout << "Init ok (myname = " << cfg.get_child("name").data() << "... sending OK" << std::endl;
 
-    // this thread need realtime priority so it doesn't miss interrupt.
-    struct sched_param p;
-    p.sched_priority = 90;
-    assert(pthread_setschedparam(pthread_self(), SCHED_FIFO, &p) == 0);
+        std::cout << "Init ok (myname = " << cfg.get_child("name").data() << "... sending OK" << std::endl;
 
-    tl_log_socket->send(zmqpp::message() << "INFO" << "BOAP CEST LINFO DU JOUR");
+        // this thread need realtime priority so it doesn't miss interrupt.
+        struct sched_param p;
+        p.sched_priority = 90;
+        assert(pthread_setschedparam(pthread_self(), SCHED_FIFO, &p) == 0);
 
-    pipe->send(zmqpp::signal::ok);
-    module.run();
-    std::cout << "module sysfsgpio shutying down" << std::endl;
+        INFO("Hello World");
+        //ERROR("HELLO WORLD 2");
+        //  tl_log_socket->send(zmqpp::message() << "INFO" << "BOAP CEST LINFO DU JOUR");
+
+        pipe->send(zmqpp::signal::ok);
+        module.run();
+        std::cout << "module sysfsgpio shutying down" << std::endl;
+    }
+        catch (std::exception &)
+                {
+                    std::this_thread::sleep_for(std::chrono::milliseconds(1000));
+                        return false;
+                }
     return true;
 }
--- src/tools/log.hpp
+++ src/tools/log.hpp
@@ -1,8 +1,8 @@
 /**
- * \file log.hpp
- * \author Thibault Schueller <ryp.sqrt@gmail.com>
- * \brief Log class declaration
- */
+* \file log.hpp
+* \author Thibault Schueller <ryp.sqrt@gmail.com>
+* \brief Log class declaration
+*/
 
 #ifndef LOG_HPP
 #define LOG_HPP
@@ -14,13 +14,173 @@
 #include "unixfs.hpp"
 #include <zmqpp/zmqpp.hpp>
 
+/**
+* This is declared here so source file including
+* log.gpp will have access to the socket.
+* It is defined in kernel.cpp
+*/
 extern thread_local zmqpp::socket *tl_log_socket;
+
+
+#include <string>
+#include <sstream>
+
+enum class LogLevel
+{
+DEBUG,
+INFO,
+WARN,
+ERROR,
+};
+
+struct LogHelper
+{
+static std::string log_level_to_string(LogLevel level)
+{
+     switch (level)
+{
+case LogLevel::DEBUG:
+return "DEBUG";
+case LogLevel::INFO:
+return "INFO";
+case LogLevel::WARN:
+return "WARNING";
+case LogLevel::ERROR:
+return "ERROR";
+}
+return "UNKNOWN";
+}
+    static void log(const std::string &log_msg, int line, const char *funcName,
+            const char *fileName, LogLevel level)
+    {
+        zmqpp::message msg;
+        //msg << fileName;
+        //msg << funcName;
+        //msg << line;
+        msg << log_level_to_string(level);
+        msg << log_msg;
+
+        tl_log_socket->send(msg);
+    }
+
+};
+
+/**
+* @def FUNCTION_NAME_MACRO
+* Macro used to get the name of the function that called the logging macro.
+* GNU compilers provide a nice macro the get a more detailed name of a function.
+* Otherwise we fallback to a more widespread macro.
+*/
+#ifdef __GNUC__
+#define FUNCTION_NAME_MACRO __PRETTY_FUNCTION__
+#else
+#define FUNCTION_NAME_MACRO __FUNCTION__
+#endif
+
+/**
+* Internal macro.
+* It is used to create an anonymous lambda to allow the logging macros parameter
+* to behave like a stringstream.
+*/
+#define BUILD(param) [&] (void) {std::stringstream ss; ss << param; return ss.str();}()
+/**
+* See "Internal macros documentation"
+*/
+#define DEBUG_0(msg) LogHelper::log(BUILD(msg), __LINE__, FUNCTION_NAME_MACRO, \
+__FILE__, LogLevel::DEBUG)
+/**
+* See "Internal macros documentation"
+*/
+#define DEBUG_1(msg, loggers) LogHelper::log(BUILD(msg), __LINE__, FUNCTION_NAME_MACRO, \
+__FILE__, LogLevel::DEBUG, loggers)
+/**
+* See "Internal macros documentation"
+*/
+#define DEBUG_X(trash, msg, loggers, targetMacro, ...) targetMacro
+/**
+* Debug macro.
+* Issue a log message with DEBUG level
+*/
+#define DEBUG(...) DEBUG_X(, ##__VA_ARGS__, \
+DEBUG_1(__VA_ARGS__), \
+DEBUG_0(__VA_ARGS__), \
+DEBUG_NO_PARAM(__VA_ARGS__), \
+)
+/**
+* See "Internal macros documentation"
+*/
+#define INFO_0(msg) LogHelper::log(BUILD(msg), __LINE__, FUNCTION_NAME_MACRO, \
+__FILE__, LogLevel::INFO)
+/**
+* See "Internal macros documentation"
+*/
+#define INFO_1(msg, loggers) LogHelper::log(BUILD(msg), __LINE__, FUNCTION_NAME_MACRO, \
+__FILE__, LogLevel::INFO, loggers)
+/**
+* See "Internal macros documentation"
+*/
+#define INFO_X(trash, msg, loggers, targetMacro, ...) targetMacro
+/**
+* Information macro.
+* Issue a log message with INFO level.
+*/
+#define INFO(...) INFO_X(, ##__VA_ARGS__, \
+INFO_1(__VA_ARGS__), \
+INFO_0(__VA_ARGS__), \
+INFO_NO_PARAM(__VA_ARGS__), \
+)
+/**
+* See "Internal macros documentation"
+*/
+#define WARN_0(msg) LogHelper::log(BUILD(msg), __LINE__, FUNCTION_NAME_MACRO, \
+__FILE__, LogLevel::WARN)
+/**
+* See "Internal macros documentation"
+*/
+#define WARN_1(msg, loggers) LogHelper::log(BUILD(msg), __LINE__, FUNCTION_NAME_MACRO, \
+__FILE__, LogLevel::WARN)
+/**
+* See "Internal macros documentation"
+*/
+#define WARN_X(trash, msg, loggers, targetMacro, ...) targetMacro
+/**
+* Warning macro.
+* Issue a log message with WARN level.
+*/
+#define WARN(...) WARN_X(, ##__VA_ARGS__, \
+WARN_1(__VA_ARGS__), \
+WARN_0(__VA_ARGS__), \
+WARN_NO_PARAM(__VA_ARGS__), \
+)
+/**
+* See "Internal macros documentation"
+*/
+#define ERROR_0(msg) LogHelper::log(BUILD(msg), __LINE__, FUNCTION_NAME_MACRO, \
+__FILE__, LogLevel::ERROR)
+/**
+* See "Internal macros documentation"
+*/
+#define ERROR_1(msg, loggers) LogHelper::log(BUILD(msg), __LINE__, FUNCTION_NAME_MACRO, \
+__FILE__, LogLevel::ERROR)
+/**
+* See "Internal macros documentation"
+*/
+#define ERROR_X(trash, msg, loggers, targetMacro, ...) targetMacro
+/**
+* Error macro.
+* Issue a log message with ERROR level
+*/
+#define ERROR(...) ERROR_X(, ##__VA_ARGS__, \
+ERROR_1(__VA_ARGS__), \
+ERROR_0(__VA_ARGS__), \
+ERROR_NO_PARAM(__VA_ARGS__), \
+)
 
 class LogLine
 {
 public:
-    LogLine(const char* file, const char* func, int line, std::ostream& out = std::cout)
-    : _out(out)
+    LogLine(const char *file, const char *func, int line, std::ostream &out = std::cout)
+            : _out(out)
     {
         _stream << UnixFs::stripPath(file) << ':' << line << ": in '" << func << "': ";
     }
@@ -32,16 +192,16 @@ public:
         _out.flush();
     }
 
-    template <class T>
-    LogLine& operator<<(const T& arg)
+    template<class T>
+    LogLine &operator<<(const T &arg)
     {
         _stream << arg;
         return (*this);
     }
 
 private:
-    std::stringstream   _stream;
-    std::ostream&       _out;
+    std::stringstream _stream;
+    std::ostream &_out;
 };
 
 #define LOG() LogLine(__FILE__, __FUNCTION__, __LINE__)
