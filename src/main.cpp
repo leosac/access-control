b5b6bf3921ca9ecaf42b8802a44e8bdf761bcf85
xaqq <kapp.arno@gmail.com>
2014-11-26 17:10:36 +0100

    simplify main() a bit

--- src/main.cpp
+++ src/main.cpp
@@ -1,8 +1,8 @@
 /**
- * \file main.cpp
- * \author Thibault Schueller <ryp.sqrt@gmail.com>
- * \brief standard main
- */
+* \file main.cpp
+* \author Thibault Schueller <ryp.sqrt@gmail.com>
+* \brief standard main
+*/
 
 #include <iostream>
 
@@ -24,59 +24,53 @@ void print_usage();
 
 using namespace Leosac::Tools;
 
-int main(int argc, char** argv)
+int main(int argc, char **argv)
 {
-    RuntimeOptions  options;
-    int             relaunch = 1;
+    RuntimeOptions options;
+    int relaunch = 1;
 
-    try {
-        try {
-            TCLAP::CmdLine                  cmd("Open Source Access Controller", ' ', Leosac::getVersionString());
-            TCLAP::SwitchArg                verboseSwitch("v", "verbose", "Increase verbosity", false);
-            TCLAP::ValueArg<std::string>    kernelFile("k", "kernel-cfg", "Kernel Configuration file", false, "", "string");
+    try
+    {
+        TCLAP::CmdLine cmd("Open Source Access Controller", ' ', Leosac::getVersionString());
+        TCLAP::SwitchArg verboseSwitch("v", "verbose", "Increase verbosity", false);
+        TCLAP::ValueArg<std::string> kernelFile("k", "kernel-cfg", "Kernel Configuration file", false, "", "string");
 
-            cmd.add(verboseSwitch);
-            cmd.add(kernelFile);
-            cmd.parse(argc, argv);
-            options.setFlag(RuntimeOptions::Verbose, verboseSwitch.getValue());
-            options.setParam("kernel-cfg", kernelFile.getValue());
-        }
-        catch (const TCLAP::ArgException &e) {
-            throw (LEOSACException(e.error()));
-        }
-        while (relaunch)
-        {
-            UnixShellScript backup("cp -f");
-
-            //backup.run(UnixShellScript::toCmdLine(options.getParam("corecfg"), "core.cfg"));
-            //backup.run(UnixShellScript::toCmdLine(options.getParam("hwcfg"), "hw.cfg"));
-
-            try
-            {
-                Kernel kernel(Kernel::make_config(options));
-                relaunch = kernel.run();
-            }
-            catch (const CoreException &e)
-            {
-                Leosac::print_exception(e);
-                print_usage();
-                return 1;
-            }
-            catch (const std::exception &e)
-            {
-                relaunch = false;
-                std::cerr << "Exception propagated to main(). Will now exit." << std::endl;
-                Leosac::print_exception(e);
-            }
-            catch (...)
-            {
-                relaunch = false;
-                std::cerr << "Unkown exception in main" << std::endl;
-            }
-        }
+        cmd.add(verboseSwitch);
+        cmd.add(kernelFile);
+        cmd.parse(argc, argv);
+        options.setFlag(RuntimeOptions::Verbose, verboseSwitch.getValue());
+        options.setParam("kernel-cfg", kernelFile.getValue());
     }
-    catch (const LEOSACException& e) {
-        std::cerr << e.what() << std::endl;
+    catch (const TCLAP::ArgException &e)
+    {
+        throw (LEOSACException(e.error()));
+    }
+    while (relaunch)
+    {
+        UnixShellScript backup("cp -f");
+
+        try
+        {
+            Kernel kernel(Kernel::make_config(options));
+            relaunch = kernel.run();
+        }
+        catch (const CoreException &e)
+        {
+            Leosac::print_exception(e);
+            print_usage();
+            return 1;
+        }
+        catch (const std::exception &e)
+        {
+            std::cerr << "Exception propagated to main(). Will now exit." << std::endl;
+            Leosac::print_exception(e);
+            return 1;
+        }
+        catch (...)
+        {
+            std::cerr << "Unkown exception in main" << std::endl;
+            return 1;
+        }
     }
     return (0);
 }
