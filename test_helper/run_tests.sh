9ec985fbd8ff16295f3b6440ede55c741dc33ae8
xaqq <kapp.arno@gmail.com>
2014-12-02 17:21:44 +0100

    Improve shell based testing.

--- test_helper/run_tests.sh
+++ test_helper/run_tests.sh
@@ -3,15 +3,31 @@
 # Run shell script based tests
 #
 
+RCol='\e[0m' # Text Reset
+Bla='\e[0;30m';
+Red='\e[0;31m';
+Gre='\e[0;32m';
+Yel='\e[0;33m';
+Blu='\e[0;34m';
+
+
+[ ! -z $1 ] && [ -r $1 ] || { echo "Need a readable install directory to use"; exit 1; }
+
+export INSTALL_DIR="$1"
+
+echo "Running tests, using $1 as install directory"
+
 for d in `find . -type d`; do
     if [ $d = "." ] || [ $d = ".." ] ; then continue; fi
-    echo "Will run test: $d"
+    echo -e ${Yel}"Will run test:${RCol} $d"
     pushd $d
     [ -x run_test.sh ] || { echo "Not run_test.sh files in subdirectory"; exit 1; }
-    ./run_test.sh || { echo "Test failed"; exit 1; }
+    ./run_test.sh || { echo -e ${Red}"Test failed"${RCol} ; exit 1; }
+    echo -e ${Gre}"Test $d succeded"${RCol}
     popd
 done
+pwd
 
-echo "All tests passed !"
+echo -e ${Gre}"All tests passed !"${RCol}
 
 exit 0
--- test_helper/test-working-directory/run_test.sh
+++ test_helper/test-working-directory/run_test.sh
@@ -1,36 +1,18 @@
 #!/bin/bash
 
-TMP_DIR=$(mktemp -d)
-set -x
-set -e
-
-echo "Working in $TMP_DIR"
-function die()
-{
-    rm -rf $TMP_DIR
-    exit $1
-}
-
-## check that we are in leosac/test_helper and that we run the script from here
-## ./test_working_directory.
-[ `dirname $0` == "." ] || die -1
-
-cp -R ../../build $TMP_DIR
-cp ../../cfg/dev-test/amd64.xml $TMP_DIR
-
-cd $TMP_DIR;
-
-mkdir -p path/to/wd
+[ -r ../shell_helper.sh ] || { echo "Cannot source shell_helper.sh"; exit -1; }
+source ../shell_helper.sh
 
 ## should fail, no valid path to plugin
-if ./build/leosac -k $TMP_DIR/amd64.xml; then
+if ./install/bin/leosac/ -k $TMP_DIR/this_test/amd64.xml; then
     echo "Leosac exited with status 0 but should have failed"
     die 1
 fi
 
 ## should work because we use the plugin dir as workding dir
 (sleep 3s; kill `pidof leosac`) &
-if ! ./build/leosac -k $TMP_DIR/amd64.xml -d $TMP_DIR/build ; then
+if ! ./install/bin/leosac -k $TMP_DIR/this_test/amd64.xml \
+    -d $TMP_DIR/install/lib/leosac ; then
     echo "Leosac failed, but should have succeded"
     die 1
 fi
