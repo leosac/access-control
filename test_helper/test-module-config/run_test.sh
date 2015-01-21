eab35935aad4c29738485f603d0129279bec8cfd
xaqq <kapp.arno@gmail.com>
2015-01-21 15:35:47 +0100

    fix

--- test_helper/test-module-config/run_test.sh
+++ test_helper/test-module-config/run_test.sh
@@ -14,5 +14,19 @@ set -x
 ## to make use of some of its code.
 cp "$SCRIPT_DIR/../../scripts/remote_control.py" .
 
+(valgrind --error-exitcode=42 ./install/bin/leosac -k $config_file > leosac-log &
+    echo $! > pid-file;  wait $! && echo $? > exit-status) &
+
 python "$SCRIPT_DIR/test-valid.py"
 python "$SCRIPT_DIR/test-invalid.py"
+
+kill $(cat pid-file)
+
+while [ ! -r exit-status ] || [ ! -n $(cat exit-status) ];
+do
+    sleep 1;
+done;
+
+[ $(cat exit-status) -eq 0 ] || { exit 1; }
+
+exit 0
