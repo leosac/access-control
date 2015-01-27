ef61ba39fb3fdb963f5d88ee891deeda332c65b3
xaqq <kapp.arno@gmail.com>
2015-01-27 16:29:03 +0100

    test default case of sync_source in config file

--- test/ConfigManager.cpp
+++ test/ConfigManager.cpp
@@ -42,11 +42,15 @@ namespace Leosac
 
                 auto cfg_tree = Tools::propertyTreeFromXmlFile(gl_data_path + "ConfigManager1.xml");
                 cfg1 = std::make_shared<ConfigManager>(cfg_tree.get_child("kernel"));
+
+                cfg_tree = Tools::propertyTreeFromXmlFile(gl_data_path + "ConfigManager2.xml");
+                cfg2 = std::make_shared<ConfigManager>(cfg_tree.get_child("kernel"));
             }
 
         protected:
             std::shared_ptr<ConfigManager> cfg0;
             std::shared_ptr<ConfigManager> cfg1;
+            std::shared_ptr<ConfigManager> cfg2;
         };
 
 
@@ -81,7 +85,7 @@ namespace Leosac
 
         /**
         * Like build_cfg test, but we build a config tree for export.
-        * We must make sure that information from `<sync_from>` are honored.
+        * We must make sure that information from `<sync_source>` are honored.
         */
         TEST_F(ConfigManagerTest, build_safe_cfg)
         {
@@ -94,6 +98,24 @@ namespace Leosac
             ASSERT_FALSE(c1);
             ASSERT_TRUE(c2);
             ASSERT_FALSE(c3);
+        }
+
+        /**
+        * Safe as build_safe_cfg but with a second config file.
+        * We do not declare <sync_source> there, so we should retrieve
+        * everything.
+        */
+        TEST_F(ConfigManagerTest, build_safe_cfg2)
+        {
+            auto cfg = cfg2->get_exportable_general_config();
+
+            auto c1 = cfg.get_child_optional("plugin_directories");
+            auto c2 = cfg.get_child_optional("log");
+            auto c3 = cfg.get_child_optional("network");
+
+            ASSERT_TRUE(c1);
+            ASSERT_TRUE(c2);
+            ASSERT_TRUE(c3);
         }
     }
 }
