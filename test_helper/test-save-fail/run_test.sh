9c27f31933c6ccc380ac887ff156bc4e701b74bb
xaqq <kapp.arno@gmail.com>
2015-01-27 17:57:03 +0100

    Save fail test use a RO loop FS instead of chattr +i.
    chattr +i was cleaner, but didn't work in all environment,
    for some sad, unknown reason :(

--- test_helper/test-save-fail/run_test.sh
+++ test_helper/test-save-fail/run_test.sh
@@ -16,17 +16,27 @@ cp "$SCRIPT_DIR/../../scripts/remote_control.py" this_test
 
 config_file="$TMP_DIR/this_test/test-save-fail.xml"
 
-chmod 400 $config_file;
-chattr +i "$config_file"
+
+## Use a RO filesystem to store the config file.
+## This way, we are sure that SAVE will fail.
+dd if=/dev/zero of=${TMP_DIR}/block bs=4096 count=1024
+mkfs.ext4 ${TMP_DIR}/block
+mkdir ${TMP_DIR}/mnt
+mount -o loop,rw ${TMP_DIR}/block ${TMP_DIR}/mnt
+cp $config_file $TMP_DIR/mnt
+mount -o remount,ro ${TMP_DIR}/block ${TMP_DIR}/mnt
+config_file="${TMP_DIR}/mnt/test-save-fail.xml"
 
 (valgrind --error-exitcode=42 ./install/bin/leosac -k $config_file &
     echo $! > pid-file;  wait $! && echo $? > exit-status) &
 
-sleep 3s;
+sleep 8s;
 
 pushd "$TMP_DIR/this_test"
 python test-save.py || { echo "Failure"; ls -l $config_file ; cat $config_file; exit 1; }
 popd
+
+sleep 4s
 
 kill $(cat pid-file)
 
