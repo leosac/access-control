b9f9fc13c03a5fc1c730670026b9cf96e7b1b8d0
xaqq <kapp.arno@gmail.com>
2015-04-01 10:08:14 +0200

    Event publisher will only watch for CP1000 encoded data

--- src/modules/event-publish/EventPublish.cpp
+++ src/modules/event-publish/EventPublish.cpp
@@ -24,19 +24,49 @@ void EventPublish::handle_msg_bus()
     std::string src;
     Leosac::Auth::SourceType type;
     std::string card;
+    int bits;
 
     INFO("Publishing card id {" << card << "}");
     bus_sub_.receive(msg);
 
     if (msg.parts() < 4)
         return;
-    msg >> src >> type >> card;
+    msg >> src >> type >> card >> bits;
 
     if (type != Leosac::Auth::SourceType::SIMPLE_WIEGAND)
     {
         INFO("EventPublish cannot handle this type of credential yet.");
         return;
     }
+
+    // Hack Alert! Hack Alert!
+    // For this use case, we read 35bits wiegand frame CP1000 encoded.
+    // We want to extract the identifier: the 20 second-to-last bits (14 first bit ignored).
+    // In case this is not a 35 bits tram, do nothing
+    if (bits == 35)
+    {
+        std::stringstream ss;
+        ss << std::hex << "0x";
+        for (auto c : card)
+        {
+            if (c == ':')
+                continue;
+            ss << std::hex << c;
+        }
+        INFO("Bla: {" << ss.str() << "}");
+        // n has 40 "meaningful" bits because the hex string was 5 bytes.
+        uint64_t n;
+        ss >> n;
+        INFO(" n = " << n);
+        n = (n >> 6); // remove last 6bits. parity bit + 5bits;
+        n &= 0xFFFFF; // keep 20 bits.
+        INFO(" n2 = " << n);
+        std::stringstream ss2;
+        ss2 << n;
+        ss2 >> card;
+    }
+    else
+        return;
 
     if (publish_source_)
         network_pub_.send(zmqpp::message() << card << src);
