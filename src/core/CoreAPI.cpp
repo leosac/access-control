woakes070048/leosac

Introduce CoreAPI, a safe wrapper around Core API.
CoreAPI is a new object that allows retrieval of information
from the Core in a safe way. It uses the tasks infrastructure
internaly to run code in the main thread, and extract
required information.

This commit also improve the shutdown process. It's possible
for shutdown to trigger a deadlock:
    + If the core waits on a module, and the module waits on a
    task that run on the main thread, we get a deadlock.
    + Similarly, if the module sends messages to the core
    while the core waits for the module to shutdown, we get
    a deadlock.

The improved Leosac::Kernel code will now *request* shutdown,
and then continue normal operations for 5 seconds before
waiting on module's shutdown. It's not perfect, but its a
step in the right direction.

src/core/CoreAPI.cpp
@@ -1,0 +1,70 @@
+/*
+    Copyright (C) 2014-2015 Islog
+
+    This file is part of Leosac.
+
+    Leosac is free software: you can redistribute it and/or modify
+    it under the terms of the GNU Affero General Public License as published by
+    the Free Software Foundation, either version 3 of the License, or
+    (at your option) any later version.
+
+    Leosac is distributed in the hope that it will be useful,
+    but WITHOUT ANY WARRANTY; without even the implied warranty of
+    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
+    GNU Affero General Public License for more details.
+
+    You should have received a copy of the GNU Affero General Public License
+    along with this program. If not, see <http://www.gnu.org/licenses/>.
+*/
+
+#include <core/tasks/GetLocalConfigVersion.hpp>
+#include "CoreAPI.hpp"
+#include "kernel.hpp"
+#include "Scheduler.hpp"
+
+using namespace Leosac;
+
+CoreAPI::CoreAPI(Kernel &k) :
+    kernel_(k)
+{
+
+}
+
+uint64_t CoreAPI::config_version() const
+{
+    auto t = std::make_shared<Tasks::GetLocalConfigVersion>(kernel_);
+    kernel_.core_utils()->scheduler().enqueue(t, TargetThread::MAIN);
+    t->wait();
+    assert(t->succeed());
+    return t->config_version_;
+}
+
+uint64_t CoreAPI::uptime() const
+{
+    using namespace std::chrono;
+    uint64_t out;
+    auto task = Tasks::GenericTask::build([&] () {
+            auto now = steady_clock::now();
+            out = duration_cast<milliseconds>(now - kernel_.start_time()).count();
+            return true;
+    });
+    kernel_.core_utils()->scheduler().enqueue(task, TargetThread::MAIN);
+    task->wait();
+    assert(task->succeed());
+
+    return out;
+}
+
+std::string CoreAPI::instance_name() const
+{
+    std::string out;
+     auto task = Tasks::GenericTask::build([&] () {
+             out = kernel_.config_manager().instance_name();
+             return true;
+    });
+    kernel_.core_utils()->scheduler().enqueue(task, TargetThread::MAIN);
+    task->wait();
+    assert(task->succeed());
+
+    return out;
+}

leosac/access-control

src/core/CoreAPI.cpp
@@ -1,0 +1,70 @@
+/*
+    Copyright (C) 2014-2015 Islog
+
+    This file is part of Leosac.
+
+    Leosac is free software: you can redistribute it and/or modify
+    it under the terms of the GNU Affero General Public License as published by
+    the Free Software Foundation, either version 3 of the License, or
+    (at your option) any later version.
+
+    Leosac is distributed in the hope that it will be useful,
+    but WITHOUT ANY WARRANTY; without even the implied warranty of
+    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
+    GNU Affero General Public License for more details.
+
+    You should have received a copy of the GNU Affero General Public License
+    along with this program. If not, see <http://www.gnu.org/licenses/>.
+*/
+
+#include <core/tasks/GetLocalConfigVersion.hpp>
+#include "CoreAPI.hpp"
+#include "kernel.hpp"
+#include "Scheduler.hpp"
+
+using namespace Leosac;
+
+CoreAPI::CoreAPI(Kernel &k) :
+    kernel_(k)
+{
+
+}
+
+uint64_t CoreAPI::config_version() const
+{
+    auto t = std::make_shared<Tasks::GetLocalConfigVersion>(kernel_);
+    kernel_.core_utils()->scheduler().enqueue(t, TargetThread::MAIN);
+    t->wait();
+    assert(t->succeed());
+    return t->config_version_;
+}
+
+uint64_t CoreAPI::uptime() const
+{
+    using namespace std::chrono;
+    uint64_t out;
+    auto task = Tasks::GenericTask::build([&] () {
+            auto now = steady_clock::now();
+            out = duration_cast<seconds>(now - kernel_.start_time()).count();
+            return true;
+    });
+    kernel_.core_utils()->scheduler().enqueue(task, TargetThread::MAIN);
+    task->wait();
+    assert(task->succeed());
+
+    return out;
+}
+
+std::string CoreAPI::instance_name() const
+{
+    std::string out;
+     auto task = Tasks::GenericTask::build([&] () {
+             out = kernel_.config_manager().instance_name();
+             return true;
+    });
+    kernel_.core_utils()->scheduler().enqueue(task, TargetThread::MAIN);
+    task->wait();
+    assert(task->succeed());
+
+    return out;
+}