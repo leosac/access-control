7b6acbad63a4f7cfb1413c10fb825ca02e2e20ea
Kapp Arnaud <kapp.arno@gmail.com>
2017-07-03 20:43:48 +0200

    [PYTHON] Missing file

---
+++ python/leosacpy/cli/dev/doc.py
@@ -0,0 +1,25 @@
+from subprocess import call
+import click
+from leosacpy.utils import guess_root_dir
+
+
+# Commands in this file are added manually
+# in dev.py
+
+
+@click.group()
+@click.pass_context
+def doc(ctx):
+    pass
+
+
+@doc.command(name='build')
+@click.pass_context
+def build(ctx):
+    """
+
+    Build Leosac documentation.
+    """
+
+    call('cd {} && doxygen doc/Doxyfile'.format(guess_root_dir()),
+         shell=True)
