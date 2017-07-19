797bd9842faa4b1cd29d713aa4721379fd56e2b6
Kapp Arnaud <kapp.arno@gmail.com>
2017-07-20 01:59:53 +0200

    [PYTHON/CLI] Add test-credentials command

--- python/leosacpy/scripts/cli.py
+++ python/leosacpy/scripts/cli.py
@@ -1,5 +1,7 @@
 import logging
 from types import SimpleNamespace
+
+import asyncio
 import click
 import colorama
 from click_repl import register_repl
@@ -10,6 +12,8 @@ from leosacpy.utils import guess_root_dir
 # This is because when running "leosaccli --debug shell"
 # we want all command running in the REPL to have DEBUG flag
 # turned on.
+from leosacpy.wsclient import LeosacAPI
+
 sticky_debug_flag = None
 
 
@@ -32,6 +36,22 @@ def cli_entry_point(ctx, debug, root_dir):
     ctx.obj.root_dir = root_dir or guess_root_dir()
     if not ctx.obj.root_dir:
         logging.warning('Running without a Leosac root directory.')
+
+@cli_entry_point.command(name='test-credentials')
+@click.argument('username')
+@click.argument('password')
+@click.pass_context
+def test_credential(ctx, username, password):
+    c = LeosacAPI(target='ws://127.0.0.1:8888')
+
+    def r():
+        c.authenticate(username, password)
+
+    v = asyncio.get_event_loop().run_until_complete(c.authenticate(username, password))
+    asyncio.get_event_loop().run_until_complete(c.close())
+
+    print('Auth result: {}'.format(v))
+
 
 cli_entry_point.add_command(leosacpy.cli.dev.dev.dev_cmd_group)
 
